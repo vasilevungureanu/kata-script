435b39e7ab6feaf9392033306bb2b3a1e20b39cd
Vasile Ungureanu <vungureanu@4suites.nl>
2019-10-14 13:11:26 +0300

    Add empty scripts

---
+++ create-jvm-kata-project.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Create Java/JVM kata project.
+
+# If a command fails then do not proceed and fail this script too
+set -o errexit
+set -o pipefail
---
+++ create-kotlin-jvm-kata-project.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Create Kotlin/JVM kata project.
+
+# If a command fails then do not proceed and fail this script too
+set -o errexit
+set -o pipefail
