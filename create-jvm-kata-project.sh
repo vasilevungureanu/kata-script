3d248c7a424738772408768323f1ed538a1eecf2
Vasile Ungureanu <vungureanu@4suites.nl>
2019-10-22 20:43:58 +0300

    Update scripts

--- create-jvm-kata-project.sh
+++ create-jvm-kata-project.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Create Java/JVM kata project.
+# Creates a Java/JVM kata project.
 
 # If a command fails then do not proceed and fail this script too
 set -o errexit
--- create-kotlin-jvm-kata-project.sh
+++ create-kotlin-jvm-kata-project.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Create Kotlin/JVM kata project.
+# Creates a Kotlin/JVM kata project.
 
 # If a command fails then do not proceed and fail this script too
 set -o errexit
